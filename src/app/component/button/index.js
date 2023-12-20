43858f1e73635096328e694be2e0f0a949d7e36b
RajaFaizan125 <faizan.akhtar@sprintx.net>
2023-12-20 20:07:05 +0500

    file convertor

--- src/app/component/button/index.js
+++ src/app/component/button/index.js
@@ -1,14 +1,24 @@
-
 import React from "react";
 import Image from "next/image";
 import buttonIcon from "../../assets/buttonIcon.png";
 
-const Button = ({handleClick , inputRef, changeHandler}) => {
-  return <div className="bg-[#FF5A5F] text-white w-max py-3 px-4 rounded-md flex cursor-pointer" onClick={handleClick} >
-    <Image src={buttonIcon} alt="plus file icon" width={25}/>
-    <span className="ml-2" >Datei auswählen</span>
-    <input type="file" className="" style={{display:"none"}} ref={inputRef} onChange={changeHandler} />
-  </div>;
+const Button = ({ handleClick, inputRef, changeHandler }) => {
+  return (
+    <div
+      className="bg-[#FF5A5F] text-white w-max py-3 px-4 rounded-md flex cursor-pointer"
+      onClick={handleClick}
+    >
+      <Image src={buttonIcon} alt="plus file icon" width={25} />
+      <span className="ml-2">Datei auswählen</span>
+      <input
+        type="file"
+        accept=".heic"
+        style={{ display: "none" }}
+        ref={inputRef}
+        onChange={changeHandler}
+      />
+    </div>
+  );
 };
 
 export default Button;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-
-"use client"
+"use client";
 import Footer from "./component/footer";
 import NavBar from "./component/nav-bar";
 
@@ -7,16 +6,59 @@ import frameImg from "./assets/Frame.png";
 import Image from "next/image";
 import Button from "./component/button";
 import { useRef, useState } from "react";
+import heic2any from "heic2any";
 
 export default function Home() {
-  const inputRef=useRef()
-  const [fileSize , setFileSize]=useState()
-  console.log("file size  : " , fileSize)
+  const inputRef = useRef();
+  const [fileSize, setFileSize] = useState();
+  console.log("file size  : ", fileSize);
 
-  const fileChangeHandler=(e)=>{
-    setFileSize((e.target.files[0].size/(1024 * 1024)).toFixed(2))
+  const fileChangeHandler = (e) => {
+    if (e?.target.files[0].name?.split(".")[1] === "heic") {
+      setFileSize((e.target.files[0].size / (1024 * 1024)).toFixed(2));
+    } else {
+      alert("please select file with HEIC extension");
+    }
+  };
 
-  }
+  const handleFileChange = async (event) => {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      // Check file extension
+      const allowedExtensions = ["heic"];
+      const fileExtension = selectedFile.name.split(".").pop().toLowerCase();
+
+      if (allowedExtensions.includes(fileExtension)) {
+        // Convert HEIC to JPG
+        const jpgBlob = await convertHeicToJpg(selectedFile);
+
+        // You can now use jpgBlob as needed (e.g., upload it to a server or display it)
+        console.log("Converted file:", jpgBlob);
+
+        // Optionally, you can download the converted file
+        downloadFile(jpgBlob, "converted.jpg");
+      } else {
+        alert("Please select a HEIC file.");
+      }
+    }
+  };
+
+  const convertHeicToJpg = async (heicFile) => {
+    return await heic2any({
+      blob: heicFile,
+      toType: "image/jpeg",
+    });
+  };
+
+  const downloadFile = (blob, fileName) => {
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <main>
@@ -34,22 +76,38 @@ export default function Home() {
           </p>
           <Image src={frameImg} alt="img" width={250} />
         </div>
-        <div className="flex justify-center my-5" >
-
-          <Button handleClick={() => inputRef?.current.click()} changeHandler={fileChangeHandler} inputRef={inputRef} />
+        <div className="flex justify-center my-5">
+          <Button
+            handleClick={() => inputRef?.current.click()}
+            // changeHandler={fileChangeHandler}
+            changeHandler={handleFileChange}
+            inputRef={inputRef}
+          />
         </div>
 
         <div className="flex gap-20 my-20 items-center">
           <div className="flex flex-col gap-2">
-            <h1 className="text-lg font-bold" >HEIC</h1>
-            <p >
-              HEIC (High Efficiency Image Format) ist ein modernes Bildformat, das sich durch die Erhaltung der Bildqualität bei gleichzeitiger Minimierung der Dateigrößen auszeichnet. Bekannt gemacht durch Apple, eignet es sich ideal für die Speichereffizienz auf ihren Geräten. Unser HEIC-zu-JPG-Konverter ermöglicht es Ihnen mühelos, HEIC-Dateien in universell kompatible JPGs umzuwandeln, damit Ihre visuellen Inhalte plattformübergreifend beeindruckend bleiben.
+            <h1 className="text-lg font-bold">HEIC</h1>
+            <p>
+              HEIC (High Efficiency Image Format) ist ein modernes Bildformat,
+              das sich durch die Erhaltung der Bildqualität bei gleichzeitiger
+              Minimierung der Dateigrößen auszeichnet. Bekannt gemacht durch
+              Apple, eignet es sich ideal für die Speichereffizienz auf ihren
+              Geräten. Unser HEIC-zu-JPG-Konverter ermöglicht es Ihnen mühelos,
+              HEIC-Dateien in universell kompatible JPGs umzuwandeln, damit Ihre
+              visuellen Inhalte plattformübergreifend beeindruckend bleiben.
             </p>
           </div>
           <div className="flex flex-col gap-2">
-            <h1 className="text-lg font-bold" >JPG</h1>
-            <p >
-            JPG (Joint Photographic Experts Group) ist bekannt für sein hervorragendes Verhältnis von Bildqualität zur Dateigröße. Es wird auf einer breiten Palette von Geräten unterstützt und ist die erste Wahl für verschiedene Zwecke. Nutzen Sie unseren HEIC-zu-JPG-Konverter, um HEIC-Dateien in dieses universell unterstützte Format umzuwandeln. Dies garantiert, dass Ihre Bilder unabhängig von Gerät oder Plattform außergewöhnlich erscheinen.
+            <h1 className="text-lg font-bold">JPG</h1>
+            <p>
+              JPG (Joint Photographic Experts Group) ist bekannt für sein
+              hervorragendes Verhältnis von Bildqualität zur Dateigröße. Es wird
+              auf einer breiten Palette von Geräten unterstützt und ist die
+              erste Wahl für verschiedene Zwecke. Nutzen Sie unseren
+              HEIC-zu-JPG-Konverter, um HEIC-Dateien in dieses universell
+              unterstützte Format umzuwandeln. Dies garantiert, dass Ihre Bilder
+              unabhängig von Gerät oder Plattform außergewöhnlich erscheinen.
             </p>
           </div>
         </div>
